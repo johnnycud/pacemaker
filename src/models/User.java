a9b64b11abaa997197a832382719777535ac7e3b
20070200 <20070200@IT118PC14.wit.ie>
2016-09-28 10:37:48 +0100

    @Override used

--- src/models/User.java
+++ src/models/User.java
@@ -1,6 +1,7 @@
 package models;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
+import com.google.common.base.Objects;
 
 public class User {
 	
@@ -13,6 +14,12 @@ public class User {
 	  {
 	  }
 
+	  @Override  
+	  public int hashCode()  
+	  {  
+	     return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);  
+	  }
+	  
 	  public User(String firstName, String lastName, String email, String password)
 	  {
 	    this.firstName = firstName;
@@ -21,6 +28,7 @@ public class User {
 	    this.password = password;
 	  }
 	  
+	  @Override
 	  public String toString()
 	  {
 		  return toStringHelper(this).addValue(firstName)
